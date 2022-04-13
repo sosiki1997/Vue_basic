1a55ac91ad0e2efd2512c0cf086067f59097bf83
sosiki1997 <3599186771@qq.com>
2022-04-13 11:39:49 +0800

    vue_basic practice

---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="demo">
+    <h2>学校名称: {{ schoolName }}</h2>
+    <h2>学校地址: {{ address }}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+// export 1.分别暴露
+// const school = Vue.extend({})
+// export default Vue.extend({});
+export default {
+  name: "School",
+  data() {
+    return {
+      schoolName: "尚硅谷",
+      address: "北京昌平",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.schoolName);
+    },
+  },
+};
+// export { school }; // 2.统一暴露
+// export default school; // 3.默认暴露
+</script>
+
+<style>
+.demo {
+  background-color: pink;
+}
+</style>
